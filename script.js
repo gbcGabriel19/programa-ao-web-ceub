1697dc4370279ec88ba7db159014289e1137d2ff
gabrielcosta <gabriel.cbarros@sempreceub.com>
2024-03-21 09:13:31 -0300

    aula03

---
+++ script.js
@@ -0,0 +1,11 @@
+let var1 =  "30";
+let var2 = 5;
+
+let resultado1 = var1 - var2;
+let resultado2 = var1 + var2;
+
+console.log(resultado1);
+console.log(typeof(resultado2));
+
+console.log(resultado2);
+console.log(typeof(resultado2));
